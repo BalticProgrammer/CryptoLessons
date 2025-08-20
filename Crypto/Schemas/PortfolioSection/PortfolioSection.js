1e1b95b998f12665b686d4de61de50f666d3fb49
Kirill <kirya.kazakov.2004@inbox.ru>
2025-08-20 19:39:49 +0400

    Task1. Добавил раздел портфель

---
+++ Crypto/Schemas/PortfolioSection/PortfolioSection.js
@@ -0,0 +1,8 @@
+define("PortfolioSection", [], function() {
+	return {
+		entitySchemaName: "Portfolio",
+		details: /**SCHEMA_DETAILS*/{}/**SCHEMA_DETAILS*/,
+		diff: /**SCHEMA_DIFF*/[]/**SCHEMA_DIFF*/,
+		methods: {}
+	};
+});
