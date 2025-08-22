86069ba6a9ff724758805c867fb3b9708e77b642
Kirill <kirya.kazakov.2004@inbox.ru>
2025-08-22 11:17:01 +0400

    Task1 Отредактировал страницу

--- Crypto/Schemas/Portfolio1Page/Portfolio1Page.js
+++ Crypto/Schemas/Portfolio1Page/Portfolio1Page.js
@@ -19,7 +19,7 @@ define("Portfolio1Page", [], function() {
 		diff: /**SCHEMA_DIFF*/[
 			{
 				"operation": "insert",
-				"name": "Name33f37aee-512f-46f6-b1b7-234a67aba849",
+				"name": "Name41a6d16b-9663-40e8-855a-5708a325ba94",
 				"values": {
 					"layout": {
 						"colSpan": 24,
@@ -36,7 +36,7 @@ define("Portfolio1Page", [], function() {
 			},
 			{
 				"operation": "insert",
-				"name": "ActualCount94734edd-8476-4e4b-bb0d-c20d38bd398d",
+				"name": "ActualCountf1d9bdfb-6f66-418d-a599-d942d5024e72",
 				"values": {
 					"layout": {
 						"colSpan": 24,
@@ -53,7 +53,7 @@ define("Portfolio1Page", [], function() {
 			},
 			{
 				"operation": "insert",
-				"name": "InvestedPercentf51a35fb-30f4-40aa-bb22-5fc05751ea07",
+				"name": "AverageBuyPricea556117c-3c96-49f9-ab37-8d47960900b9",
 				"values": {
 					"layout": {
 						"colSpan": 24,
@@ -62,11 +62,266 @@ define("Portfolio1Page", [], function() {
 						"row": 2,
 						"layoutName": "ProfileContainer"
 					},
-					"bindTo": "InvestedPercent"
+					"bindTo": "AverageBuyPrice"
 				},
 				"parentName": "ProfileContainer",
 				"propertyName": "items",
 				"index": 2
+			},
+			{
+				"operation": "insert",
+				"name": "AverageSellPrice337628ce-48d0-4b66-bdef-026956066192",
+				"values": {
+					"layout": {
+						"colSpan": 24,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 3,
+						"layoutName": "ProfileContainer"
+					},
+					"bindTo": "AverageSellPrice"
+				},
+				"parentName": "ProfileContainer",
+				"propertyName": "items",
+				"index": 3
+			},
+			{
+				"operation": "insert",
+				"name": "AveragePurchasePrice793c6553-2127-4722-b1cf-358ee512d21e",
+				"values": {
+					"layout": {
+						"colSpan": 24,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 4,
+						"layoutName": "ProfileContainer"
+					},
+					"bindTo": "AveragePurchasePrice"
+				},
+				"parentName": "ProfileContainer",
+				"propertyName": "items",
+				"index": 4
+			},
+			{
+				"operation": "insert",
+				"name": "AverageFixationPrice294682f6-514e-4de2-a591-87c2d59306b0",
+				"values": {
+					"layout": {
+						"colSpan": 24,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 5,
+						"layoutName": "ProfileContainer"
+					},
+					"bindTo": "AverageFixationPrice"
+				},
+				"parentName": "ProfileContainer",
+				"propertyName": "items",
+				"index": 5
+			},
+			{
+				"operation": "insert",
+				"name": "Codebdf333fa-c89a-48f6-a4fe-e095a01f5b97",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 0,
+						"layoutName": "Header"
+					},
+					"bindTo": "Code"
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "Symbol1061ea38-f299-4ad1-ab2e-728c7ed4cbdc",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 12,
+						"row": 0,
+						"layoutName": "Header"
+					},
+					"bindTo": "Symbol"
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 1
+			},
+			{
+				"operation": "insert",
+				"name": "Priorityde68cdc6-c3d4-4a69-8acb-da04e7783b9d",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 12,
+						"row": 1,
+						"layoutName": "Header"
+					},
+					"bindTo": "Priority"
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 2
+			},
+			{
+				"operation": "insert",
+				"name": "CurrentRatefe74a59b-76b7-4d5a-ba33-93d2a047692e",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 12,
+						"row": 2,
+						"layoutName": "Header"
+					},
+					"bindTo": "CurrentRate"
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 3
+			},
+			{
+				"operation": "insert",
+				"name": "PercentageDifference96c42900-3f8b-4afe-811d-64d67230efc4",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 12,
+						"row": 3,
+						"layoutName": "Header"
+					},
+					"bindTo": "PercentageDifference"
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 4
+			},
+			{
+				"operation": "insert",
+				"name": "PossibleX01a6dae6-16a3-4c07-9ffa-ddecaf6beb79",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 12,
+						"row": 4,
+						"layoutName": "Header"
+					},
+					"bindTo": "PossibleX"
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 5
+			},
+			{
+				"operation": "insert",
+				"name": "InvestedPercent6e24c6c7-6126-46aa-9d8c-36103baad8b8",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 12,
+						"row": 5,
+						"layoutName": "Header"
+					},
+					"bindTo": "InvestedPercent"
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 6
+			},
+			{
+				"operation": "insert",
+				"name": "InvestedUsdt29581493-947e-4ad8-9621-727412e37676",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 12,
+						"row": 6,
+						"layoutName": "Header"
+					},
+					"bindTo": "InvestedUsdt"
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 7
+			},
+			{
+				"operation": "insert",
+				"name": "InvestedRub6c3e7333-d610-4b6f-b420-5f9daa0fe541",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 12,
+						"row": 7,
+						"layoutName": "Header"
+					},
+					"bindTo": "InvestedRub"
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 8
+			},
+			{
+				"operation": "insert",
+				"name": "Notesfad2a8f4-d04c-426a-8237-b81a38f315d4",
+				"values": {
+					"layout": {
+						"colSpan": 24,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 8,
+						"layoutName": "Header"
+					},
+					"bindTo": "Notes"
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 9
+			},
+			{
+				"operation": "insert",
+				"name": "TotalCountBuy948dbe29-6fa6-4bb0-8a49-1c6e9c78abd6",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 3,
+						"layoutName": "Header"
+					},
+					"bindTo": "TotalCountBuy"
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 10
+			},
+			{
+				"operation": "insert",
+				"name": "TotalCountSell6c336ea8-0d6b-479b-a2f1-c6d8fed10e68",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 4,
+						"layoutName": "Header"
+					},
+					"bindTo": "TotalCountSell"
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 11
 			},
 			{
 				"operation": "insert",
